7a6442d04a64fd31c8cba23f68b31303cbce766c
leogcchen <leogcchen@tencent.com>
2023-12-06 00:34:01 +0800

    bugfix

--- src/clock/interfaces/clock.interface.ts
+++ src/clock/interfaces/clock.interface.ts
@@ -52,7 +52,7 @@ export const LOW = Gpio.LOW;
 
 export enum Digit {
   zero = 0, one = 1, two   = 2, three = 3, four = 4,
-  five = 5, six = 6, seven = 7, eight = 7, nine = 9,
+  five = 5, six = 6, seven = 7, eight = 8, nine = 9,
 }
 
 export const kDigitValues: {[key: number]: {[key: string]: BinaryValue}} = {};
